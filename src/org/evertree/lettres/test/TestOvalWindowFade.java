fc0d8921b1716c8d1001cfbd1d1d5e8539eb728f
Paulo Guilherme Santiago <37718188+Paulogsdv@users.noreply.github.com>
2020-10-14 22:37:24 -0300

    Feat: Commit Initial

---
+++ src/org/evertree/lettres/test/TestOvalWindowFade.java
@@ -0,0 +1,62 @@
+package org.evertree.lettres.test;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.geom.Ellipse2D;
+
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JFrame;
+import javax.swing.JProgressBar;
+import javax.swing.JRadioButton;
+import javax.swing.SwingUtilities;
+import javax.swing.Timer;
+ 
+public class TestOvalWindowFade extends JFrame {
+ 
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	final Timer fadeTimer = new Timer(100, new ActionListener() {
+ 
+        float alpha = 0f;
+ 
+        public void actionPerformed(ActionEvent e) {
+            alpha = Math.min(alpha + .1f, 1.0f);
+ 
+ 
+            if (alpha >= 1.0f) {
+                alpha = 0f;
+                ((Timer) e.getSource()).stop();
+            }
+        }
+    });
+ 
+    public TestOvalWindowFade() {
+        super("Test translucent and oval-shaped window");
+        setLayout(new java.awt.GridBagLayout());
+        add(new JButton("test"));
+        add(new JCheckBox("test"));
+        add(new JRadioButton("test"));
+        add(new JProgressBar(0, 100));
+ 
+        setSize(new java.awt.Dimension(400, 300));
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setUndecorated(true);
+ 
+    }
+ 
+    public void addNotify() {
+        super.addNotify();
+        fadeTimer.start();
+    }
+ 
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new TestOvalWindowFade().setVisible(true);
+            }
+        });
+    }
+}
